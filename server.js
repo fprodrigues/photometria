6259529ec207a797b3985dfcead8586cd83ffe30
fprodrigues <dombalder@gmail.com>
2016-10-12 17:32:17 -0300

    install packages

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var tasks = require('./routes/fundos');
+
+var port = 3000;
+
+var app = express();
+
+// view engine
+app.set('views', path.join(__dirname,'views'));
+app.set('views engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+ // set Static Folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// body parser mw
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// route
+app.use('/', index);
+app.use('/', fundos);
+
+app.listen(port, function(){
+console.log('Server started on port' + port);
+});
