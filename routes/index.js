bdb25018eb2ca29f6b1aba931eea4d0c4970119d
fprodrigues <dombalder@gmail.com>
2016-10-18 13:29:47 -0200

    new  typescript

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 //set home page
 router .get('/', function (req,  res,  next){
-	res.send('INDEX  PAGE');
-	//res.render('index.html');
+	res.render('index.html');
 });
 
 module.exports = router;
